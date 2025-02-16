0f74bcddb142eea39266385b708bf9f094825a58
ripionck <ripion.chakmar@gmail.com>
2025-02-16 09:05:48 +0600

    added transactions routes

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/auth');
-const {
-  getTransactions,
-  createTransaction,
-} = require('../controllers/transactionController');
+const transactionController = require('../controllers/transactionController');
+const authMiddleware = require('../middleware/authMiddleware');
 
-router
-  .route('/')
-  .get(protect, getTransactions)
-  .post(protect, createTransaction);
+// All routes below this line will require authentication
+router.get('/', authMiddleware, transactionController.getAllTransactions);
+router.post('/', authMiddleware, transactionController.createTransaction);
 
 module.exports = router;
