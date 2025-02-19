b9fbea28b76721cccb113106ba3aee56cdb3652d
ripionck <ripion.chakmar@gmail.com>
2025-02-20 03:28:46 +0600

    added auth to retrieves categories

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const categoryController = require('../controllers/categoryControllers');
 const authMiddleware = require('../middleware/auth');
 
-router.get('', categoryController.getAllCategories);
+router.get('', authMiddleware, categoryController.getAllCategories);
 router.post('', authMiddleware, categoryController.createCategory);
 router.put('/:id', authMiddleware, categoryController.updateCategory);
 router.delete('/:id', authMiddleware, categoryController.deleteCategory);
