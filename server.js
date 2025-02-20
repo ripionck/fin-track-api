371eeaf5c06294ee2f048520b624667b4b35f29f
ripionck <ripion.chakmar@gmail.com>
2025-02-20 08:23:03 +0600

    fixed cors origin for avatar upload

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(morgan('dev'));
 app.use(
   cors({
     origin: ['https://fin-track-azure.vercel.app', 'http://localhost:5173'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   }),
